26659bd01128772440ba29908666345859ca9485
Santiago Ochoa Paipilla <santiagop200611@gmail.com>
2025-10-23 00:25:58 -0500

    Se creo y se finalizo en frontend del login, tener en cuenta que aun no se define el accion del formulario

---
+++ views/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="full-login">
+        <div class="container-loging">
+            <div class="container-logo">
+                <div class="logo-gest">
+                    <img src="../public/images/logo-gestor.png" alt="logo-gestor">
+                </div>
+                <h1>NotasApp</h1>
+                <p>Sistema de Gestión de Notas</p>
+            </div>
+            <form action="">
+                <div class="space">
+                    <label for="username">Usuario</label>
+                    <input type="text" id="username" placeholder="Ingrese su usuario">
+                </div>
+                <div class="space">
+                    <label for="password">Contraseña</label>
+                    <input type="password" id="password" placeholder="Ingrese su contraseña">
+                </div>
+                <button class="button-log" type="submit">Ingresar</button>
+            </form>
+            <div class="center-text">
+                <p class="not-forget">No olvides tu contraseña (¬‿¬)</p>
+            </div>
+        </div>
+        <div class="center-text out">
+            <p>© 2025 NotasApp - Sistema de Gestión Académica</p>
+        </div>
+    </div>
+    
+</body>
+</html>
